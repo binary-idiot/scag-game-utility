5be397093ef126bd5efc9062c02ad13ee90023f6
Jonah Middleton <jonah.middleton@gmail.com>
2026-03-21 20:13:22 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <h1>SCAG Game Utility</h1>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+  header {
+    padding: 20px;
+  }
+</style>
