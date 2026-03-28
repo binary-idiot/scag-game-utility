b011dea4c00fccdd4d075957a0f445cafdd74d8c
Jonah Middleton <jonah.middleton@gmail.com>
2026-03-28 03:49:21 -0400

    Create Initial signin flow

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,96 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { loadScript } from 'vue-plugin-load-script'
 
+declare const gapi: any
+declare const google: any
+
+const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4'
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+
+const GAPIKey: string = import.meta.env.VITE_GOOGLE_API_KEY
+const GClientId: string = import.meta.env.VITE_GOOGLE_CLIENT_ID
+
+let tokenClient: any
+let gapiInited: boolean = false
+let gisInited: boolean = false
+
+const authEnabled = ref(false)
+const signoutEnabled = ref()
+
+function maybeEnableAuth(): void {
+  if (gapiInited && gisInited) {
+    authEnabled.value = true
+  }
+}
+
+function handleAuthClick(): void {
+  tokenClient.callback = async (response: any) => {
+    if (response.error !== undefined) {
+      throw response
+    }
+    signoutEnabled.value = true
+  }
+
+  if (gapi.client.getToken() === null) {
+    tokenClient.requestAccessToken({ prompt: 'consent' })
+  } else {
+    tokenClient.requestAccessToken({ prompt: '' })
+  }
+}
+
+function handleSignOutClick(): void {
+  const token = gapi.client.getToken()
+  if (token !== null) {
+    google.accounts.oauth2.revoke(token.access_token)
+    gapi.client.setToken('')
+    signoutEnabled.value = false
+  }
+}
+
+async function initializeGAPIClient(): Promise<void> {
+  await gapi.client.init({
+    apiKey: GAPIKey,
+    discoveryDocs: [DISCOVERY_DOC],
+  })
+  gapiInited = true
+  maybeEnableAuth()
+}
+
+onMounted(() => {
+  loadScript('https://accounts.google.com/gsi/client').then(() => {
+    tokenClient = google.accounts.oauth2.initTokenClient({
+      client_id: GClientId,
+      scope: SCOPES,
+      callback: '',
+    })
+    gisInited = true
+    maybeEnableAuth()
+  })
+
+  loadScript('https://apis.google.com/js/api.js').then(() => {
+    gapi.load('client', initializeGAPIClient)
+  })
+})
 </script>
 
 <template>
   <header>
     <h1>SCAG Game Utility</h1>
+
+    <button v-bind:disabled="!authEnabled" @click="handleAuthClick" id="auth-btn">
+      {{ signoutEnabled ? 'Refresh' : 'Authorize' }}
+    </button>
+    <button v-bind:disabled="!signoutEnabled" @click="handleSignOutClick" id="signout-btn">
+      SignOut
+    </button>
   </header>
 
-  <main>
-  </main>
+  <main></main>
 </template>
 
 <style scoped>
-  header {
-    padding: 20px;
-  }
+header {
+  padding: 20px;
+}
 </style>
